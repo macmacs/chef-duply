6270fe5046fff1b783ea388a5cb73acbfdfdecd4
Ted Chen <ted@nephilagraphic.com>
2014-03-22 04:21:16 -0300

    Rolling back version numbers

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ted@nephilagraphic.com'
 license          'All rights reserved'
 description      'Installs/Configures duply'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
 
 %w{ ubuntu debian }.each do |os|
   supports os
